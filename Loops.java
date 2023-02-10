dc6ffdb00eea664d93fcc3379f163d5beb41c8e0
JakeClise <jhclise@gmail.com>
2023-02-10 12:51:47 -0500

    first commit

---
+++ Loops.java
@@ -0,0 +1,37 @@
+public class Loops {
+    public static void main(String[] args){
+        //for loop (increase by 1) Desired output: 01234
+        //for (int i = 0; i<5; i++){
+            //System.out.println(i);
+        //}
+
+        //for loop (increase by 2)
+        //for(int i = 0; i < 5; i+=2){
+            //System.out.println(i);  
+        //}
+
+        //for loop (decrease by 1)
+        //for (int i = 5; i > -1; i --){
+        //    System.out.println(i);
+        //}
+        //While Loops
+        //int count = 0;
+        //while(count<5) {
+        //    System.out.println(count);
+        //    count++;
+        //}
+
+        //While greater than 4 and less than 30
+        //int newCount = 4;
+        //while(newCount<30) {
+        //    System.out.println(newCount);
+        //    newCount++;
+        //}
+
+        //Enhanced For Loops
+        String[] fruits = {"Apple", "Orange", "Banana", "Papaya"};
+        for(String i : fruits){
+            System.out.println(i);
+        }
+    }
+}
